539b7328960a05f31446621859bbd2732c346266
Abdul Kadir <kadirsaifi08@gmail.com>
2026-01-01 16:17:39 +0400

    update kitchen component

--- src/component/pages/service/kitchen/Kitchen.js
+++ src/component/pages/service/kitchen/Kitchen.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./Kitchen.css";
+import Footer from "../../footer/Footer";
 
 const Kitchen = () => {
   return (
+    <>
     <section className="kitchen-page">
       {/* Hero Section */}
       <div className="kitchen-hero">
@@ -18,7 +20,7 @@ const Kitchen = () => {
         {/* Left Image */}
         <div className="kitchen-image">
           <img
-            src="https://via.placeholder.com/500x400"
+            src="https://www.decorilla.com/online-decorating/wp-content/uploads/2016/02/Online-interior-designer-kitchen.jpg"
             alt="Modular Kitchen"
           />
         </div>
@@ -47,13 +49,15 @@ const Kitchen = () => {
       <div className="kitchen-gallery">
         <h2>Our Recent Kitchen Projects</h2>
         <div className="gallery-grid">
-          <img src="https://via.placeholder.com/400x300" alt="Kitchen 1" />
-          <img src="https://via.placeholder.com/400x300" alt="Kitchen 2" />
-          <img src="https://via.placeholder.com/400x300" alt="Kitchen 3" />
-          <img src="https://via.placeholder.com/400x300" alt="Kitchen 4" />
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYaFqDpN1JoTaOE9Cpq7Pk9UJYSZTvIX_WzA&s" alt="Kitchen 1" />
+          <img src="https://media.designcafe.com/wp-content/uploads/2021/09/27162134/kitchen-cabinet-dimensions.jpg" alt="Kitchen 2" />
+          <img src="https://d3gq2merok8n5r.cloudfront.net/abhinav/ond-1634120396-Obfdc/1-2025-1736068988-NDPD1/jfm-1736069001-9OxTK/kitchen-1736069015-B2aEs/kit-01-1741331976-clHbr.png" alt="Kitchen 3" />
+          <img src="https://d3gq2merok8n5r.cloudfront.net/abhinav/ond-1634120396-Obfdc/amj-2023-1678081565-AJpiy/kitchen-1678169970-j7P48/d-inki-amj2023-141-1678369598-tAMlJ/15-6-1678369604-NiorN.jpg" alt="Kitchen 4" />
         </div>
       </div>
     </section>
+    <Footer/>
+    </>
   );
 };
 
